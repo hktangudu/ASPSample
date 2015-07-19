cf0c0b786d56976626165d77f48bade7e3aa104d
hari kumar <hktangudu@gmail.com>
2015-07-19 22:55:52 +0530

    sgsg

---
+++ SampleApp/SampleApp/Login.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SampleApp.Login" %>
+
+<%@ Register assembly="DevExpress.Web.v14.2, Version=14.2.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web" tagprefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 507px" id="txtUserName">
+    <form id="form1" runat="server">
+        <div style="height: 138px">
+            <h1 style="margin-left: 240px">&nbsp;</h1>
+            <h1 style="width: 406px; margin-left: 240px">Library Management System</h1>
+        </div>
+        <div style="height: 564px">
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ SampleApp/SampleApp/Login.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace SampleApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DBCode();
+        }
+
+
+        private void DBCode()
+        {
+            SqlConnection connection = new SqlConnection("server = .\\sqlexpress;database = CMS; uid = sa; password = pass@word1");
+
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                cmd.Connection = connection;
+
+                cmd.CommandText = "select count(*) from Student";
+
+                cmd.Connection.Open();
+
+                int result = (int)cmd.ExecuteScalar();
+
+                //txtUName.Text = result.ToString();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (cmd.Connection != null && cmd.Connection.State == System.Data.ConnectionState.Open)
+                {
+                    cmd.Connection.Close();
+                }
+            }
+
+        }
+    }
+}
